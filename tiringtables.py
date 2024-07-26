d4deeaf9326f596f43568464338676e3f6d48079
sujayssa13 <sujay.ssa13@gmail.com>
2024-07-26 15:00:53 +0530

    Create tiringtables.py

---
+++ tiringtables.py
@@ -0,0 +1,8 @@
+#printable tables
+print("a     b      a**b")
+print("1     2      ", 1**2)
+print("2     3      ", 2**3)
+print("3     4      ", 3**4)
+print("4     5      ", 4**5)
+print("5     6      ", 5**6)
+print("6     7      ", 6**7)
