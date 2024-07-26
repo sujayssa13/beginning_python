30c56c394650104b476450d4148303e472ebed07
sujayssa13 <sujay.ssa13@gmail.com>
2024-07-26 11:37:58 +0530

    Create conversionInMeters.py

---
+++ conversionInMeters.py
@@ -0,0 +1,6 @@
+#converting feet in to meters
+# one Foot = 0.305 meters
+measureInFeet = eval(input("Enter the measure to be converted in meters : "))
+measureInmeters = measureInFeet *0.305
+
+print("The measurement in meters is : ",measureInmeters)
