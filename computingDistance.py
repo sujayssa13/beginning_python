da718ecc7134631bb47ffc8b0a9b6e5d901434f5
sujayssa13 <sujay.ssa13@gmail.com>
2024-07-26 11:12:04 +0530

    Create computingDistance.py

---
+++ computingDistance.py
@@ -0,0 +1,8 @@
+#compute distance
+x1,y1 = eval(input("Enter x1 and y1 for point 1: "))
+
+x2,y2 = eval(input("Enter x2 and y2 for point 2: "))
+
+distance = ((x2-x1)**2 +(y2-y1)**2)**0.5
+
+print("The distace measured is :", distance)
