a6c7c57b9391435e3a20b88a994a6a6710633cec
sujayssa13 <sujay.ssa13@gmail.com>
2024-07-26 12:57:30 +0530

    Create anoterversionofconvertingnumberintosecondsminutesandhours.py

---
+++ anoterversionofconvertingnumberintosecondsminutesandhours.py
@@ -0,0 +1,8 @@
+#converting entered number from number of seconds to minutes, hours, days and weeks
+seconds = eval(input("Enter a number that indicates number of seconds : "))
+inMinutes = seconds//60
+print("Minutes is : ", inMinutes)
+inHours = inMinutes//60
+print("in hours is : ", inHours)
+inDays = inHours//24
+print("In days : ",inDays)
