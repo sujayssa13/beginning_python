05428343ea025ae638f5fa67ca11a9408fb5319b
sujayssa13 <sujay.ssa13@gmail.com>
2024-07-26 14:43:17 +0530

    Create accelerationcalcualtion.py

---
+++ accelerationcalcualtion.py
@@ -0,0 +1,7 @@
+# acceleration
+initialVelocity = eval(input("Enter initial velocity: "))
+finalVelocity = eval(input("Enter final velocity: "))
+timeSpan = eval(input("Enter the time span  :"))
+
+acceleration = (finalVelocity-initialVelocity)/timeSpan
+print("Acceleration =", acceleration)
