67a9bc0f79a7b68d9e25ce6bfa53b228c6d730d0
sujayssa13 <sujay.ssa13@gmail.com>
2024-07-26 12:06:04 +0530

    Create retardedUnitsToKilogram.py

---
+++ retardedUnitsToKilogram.py
@@ -0,0 +1,8 @@
+#pounds to kilogram
+# one pound  = 0.454
+#measure in retarded units
+
+measureInPounds = eval(input("Enter the measure in pounds : "))
+kilogramConversion = measureInPounds * 0.454
+
+print("The measurement of Pounds", measureInPounds,"into Kiliogram is : ", kilogramConversion)
